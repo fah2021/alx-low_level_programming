d90968acb62b49cbfcb9ca15ba41dda6ae407c28
Farai Jawi <fahjawi@gmail.com>
2022-12-15 10:41:14 -0800

    first commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigital(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int sixe);
+
+void print_number(int n);
+
+
